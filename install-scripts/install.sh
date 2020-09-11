f0f389c1698d2c60f346383b3e989f26dcb0165a
Chris Priest <chris.priest@me.com>
2020-09-11 08:43:07 +0100

    add support for a single deployment target

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -5,6 +5,7 @@ SERVER2=$2
 SERVER3=$3
 LB_RANGE_START=$4
 LB_RANGE_END=$5
+SINGLE_DEPLOY_TARGET=$6
 
 IFS=':' read server_name server_ip <<< "$SERVER1"
 SERVER1_NAME=$server_name
@@ -25,15 +26,19 @@ echo "Generating config files..."
 for server_pair in "${server_pairs[@]}"
 do
 	IFS=':' read THIS_SERVER_NAME THIS_SERVER_IP <<< "$server_pair"
-	m4 -D __THIS_SERVER_NAME__=$THIS_SERVER_NAME \
-		-D __THIS_SERVER_IP__=$THIS_SERVER_IP \
-		-D __SERVER1_NAME__=$SERVER1_NAME \
-		-D __SERVER2_NAME__=$SERVER2_NAME \
-		-D __SERVER3_NAME__=$SERVER3_NAME \
-		-D __SERVER1_IP__=$SERVER1_IP \
-		-D __SERVER2_IP__=$SERVER2_IP \
-		-D __SERVER3_IP__=$SERVER3_IP \
-		etcd.default.template > $THIS_SERVER_NAME.etcd.default
+
+	if [[ -z $SINGLE_DEPLOY_TARGET || (! -z $SINGLE_DEPLOY_TARGET && $THIS_SERVER_NAME = $SINGLE_DEPLOY_TARGET) ]]
+	then
+		m4 -D __THIS_SERVER_NAME__=$THIS_SERVER_NAME \
+			-D __THIS_SERVER_IP__=$THIS_SERVER_IP \
+			-D __SERVER1_NAME__=$SERVER1_NAME \
+			-D __SERVER2_NAME__=$SERVER2_NAME \
+			-D __SERVER3_NAME__=$SERVER3_NAME \
+			-D __SERVER1_IP__=$SERVER1_IP \
+			-D __SERVER2_IP__=$SERVER2_IP \
+			-D __SERVER3_IP__=$SERVER3_IP \
+			etcd.default.template > $THIS_SERVER_NAME.etcd.default
+	fi
 done
 
 m4 -D __LB_RANGE_START__=$LB_RANGE_START \
@@ -45,99 +50,121 @@ m4 -D __LB_RANGE_START__=$LB_RANGE_START \
 
 for server_name in "${server_names[@]}"
 do	
-	echo "Waiting for ssh on $server_name to become available..."
-	until $(ssh -o ConnectTimeout=1 root@$server_name "echo")
+	if [[ -z $SINGLE_DEPLOY_TARGET || (! -z $SINGLE_DEPLOY_TARGET && $server_name = $SINGLE_DEPLOY_TARGET) ]]
+	then
+		echo "Waiting for ssh on $server_name to become available..."
+		until $(ssh -o ConnectTimeout=1 root@$server_name "echo")
+		do
+		  printf .
+		  sleep 1
+		done
+
+		echo "Waiting for apt to stop running on $server_name..."
+		until $(ps aux | grep -v grep | grep -q apt)
+		do
+		  printf .
+		  sleep 1
+		done
+
+		echo "Copying etcd config..."
+		scp $server_name.etcd.default root@$server_name:/etc/default/etcd
+
+		echo "Disable auto-start of etcd..."
+		scp policy-rc.d root@$server_name:/usr/sbin/policy-rc.d
+		ssh root@$server_name chmod +x /usr/sbin/policy-rc.d
+
+		echo "Installing gnupg..."
+		ssh root@$server_name apt-get install -y gnupg
+
+		echo "Updating apt for gluster..."
+		scp gluster8-rsa.pub root@$server_name:/root/gluster8-rsa.pub
+		ssh root@$server_name 'apt-key add /root/gluster8-rsa.pub'
+		scp gluster.list root@$server_name:/etc/apt/sources.list.d/gluster.list
+		ssh root@$server_name 'apt-get update'
+
+		echo "Installing software..."
+		ssh root@$server_name 'apt-get -o DPkg::Options::="--force-confold" -y install etcd wget curl ntp xfsprogs glusterfs-server nfs-ganesha nfs-ganesha-gluster'
+	fi
+done
+
+for server_name in "${server_names[@]}"
+do
+	if [[ -z $SINGLE_DEPLOY_TARGET || (! -z $SINGLE_DEPLOY_TARGET && $server_name = $SINGLE_DEPLOY_TARGET) ]]
+	then
+		echo "Starting etcd on $server_name..."
+		ssh root@$server_name '/etc/init.d/etcd start' &
+		ssh root@$server_name 'rm -f /usr/sbin/policy-rc.d'
+	fi
+done
+
+for server_name in "${server_names[@]}"
+do	
+	if [[ -z $SINGLE_DEPLOY_TARGET || (! -z $SINGLE_DEPLOY_TARGET && $server_name = $SINGLE_DEPLOY_TARGET) ]]
+	then
+		echo "Installing glusterfs on $server_name..."
+		scp gluster-install.sh root@$server_name:/root/gluster-install.sh
+		ssh root@$server_name chmod +x /root/gluster-install.sh
+		ssh root@$server_name /root/gluster-install.sh
+	fi
+done
+
+if [[ -z $SINGLE_DEPLOY_TARGET ]]
+then
+	echo "Setting up glusterfs volume (via $SERVER1_NAME)..."
+	scp gluster-setup-volume.sh root@$SERVER1_NAME:/root/gluster-setup-volume.sh
+	ssh root@$SERVER1_NAME chmod +x /root/gluster-setup-volume.sh
+	ssh root@$SERVER1_NAME /root/gluster-setup-volume.sh
+fi
+
+for server_name in "${server_names[@]}"
+do
+	if [[ -z $SINGLE_DEPLOY_TARGET || (! -z $SINGLE_DEPLOY_TARGET && $server_name = $SINGLE_DEPLOY_TARGET) ]]
+	then
+		echo "Setting up Gasnesha NFS on $server_name..."
+		scp ganesha-setup.sh root@$server_name:/root/ganesha-setup.sh
+		ssh root@$server_name chmod +x /root/ganesha-setup.sh
+		ssh root@$server_name /root/ganesha-setup.sh $server_name
+	fi
+done
+
+for server_name in "${server_names[@]}"
+do	
+	if [[ -z $SINGLE_DEPLOY_TARGET || (! -z $SINGLE_DEPLOY_TARGET && $server_name = $SINGLE_DEPLOY_TARGET) ]]
+	then
+		echo "Installing k3s on $server_name..."
+		ssh root@$server_name "curl -sfL https://raw.githubusercontent.com/cjrpriest/k3s-glusterfs/release-1.18/install.sh | INSTALL_K3S_VERSION=v1.18.8+k3s1+glusterfs INSTALL_K3S_EXEC=\"server --disable traefik\" sh -s - server --datastore-endpoint http://$SERVER1_IP:2379,http://$SERVER2_IP:2379,http://$SERVER3_IP:2379"
+	fi
+done
+
+if [[ -z $SINGLE_DEPLOY_TARGET ]]
+then
+	echo "Getting kube config file for this cluster..."
+	until $(scp -o ConnectTimeout=1 -q root@$SERVER1_NAME:/etc/rancher/k3s/k3s.yaml ~/.kube/config)
 	do
 	  printf .
 	  sleep 1
 	done
 
-	echo "Waiting for apt to stop running on $server_name..."
-	until $(ps aux | grep -v grep | grep -q apt)
-	do
-	  printf .
-	  sleep 1
-	done
+	echo "Modifying local kube config..."
+	sed -e "s/127\.0\.0\.1:6443/$SERVER1_NAME:6443/g" -i '' ~/.kube/config
 
-	echo "Copying etcd config..."
-	scp $server_name.etcd.default root@$server_name:/etc/default/etcd
+	echo "Deploying Metal LB..."
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+	kubectl apply -f metallb-config.yml
 
-	echo "Disable auto-start of etcd..."
-	scp policy-rc.d root@$server_name:/usr/sbin/policy-rc.d
-	ssh root@$server_name chmod +x /usr/sbin/policy-rc.d
+	echo "Deploying k8s dashboard..."
+	kubectl create -f dashboard-2.0.3-recommended.yaml
+	kubectl create -f dashboard.admin-user.yml -f dashboard.admin-user-role.yml
 
-	echo "Installing gnupg..."
-	ssh root@$server_name apt-get install -y gnupg
+	echo "Deploying gluster service and endpoints..."
+	kubectl apply -f gluster-endpoints.yml
+	kubectl apply -f gluster-service.yml
 
-	echo "Updating apt for gluster..."
-	scp gluster8-rsa.pub root@$server_name:/root/gluster8-rsa.pub
-	ssh root@$server_name 'apt-key add /root/gluster8-rsa.pub'
-	scp gluster.list root@$server_name:/etc/apt/sources.list.d/gluster.list
-	ssh root@$server_name 'apt-get update'
+	echo "Deploying demo application..."
+	kubectl apply -f nginx.yml
 
-	echo "Installing software..."
-	ssh root@$server_name 'apt-get -o DPkg::Options::="--force-confold" -y install etcd wget curl ntp xfsprogs glusterfs-server nfs-ganesha nfs-ganesha-gluster'
-done
-
-for server_name in "${server_names[@]}"
-do	
-	echo "Starting etcd on $server_name..."
-	ssh root@$server_name '/etc/init.d/etcd start' &
-	ssh root@$server_name 'rm -f /usr/sbin/policy-rc.d'
-done
-
-for server_name in "${server_names[@]}"
-do	
-	echo "Installing glusterfs on $server_name..."
-	scp gluster-install.sh root@$server_name:/root/gluster-install.sh
-	ssh root@$server_name chmod +x /root/gluster-install.sh
-	ssh root@$server_name /root/gluster-install.sh
-done
-
-echo "Setting up glusterfs volume (via $SERVER1_NAME)..."
-scp gluster-setup-volume.sh root@$SERVER1_NAME:/root/gluster-setup-volume.sh
-ssh root@$SERVER1_NAME chmod +x /root/gluster-setup-volume.sh
-ssh root@$SERVER1_NAME /root/gluster-setup-volume.sh
-
-for server_name in "${server_names[@]}"
-do
-	echo "Setting up Gasnesha NFS on $server_name..."
-	scp ganesha-setup.sh root@$server_name:/root/ganesha-setup.sh
-	ssh root@$server_name chmod +x /root/ganesha-setup.sh
-	ssh root@$server_name /root/ganesha-setup.sh $server_name
-done
-
-for server_name in "${server_names[@]}"
-do	
-	echo "Installing k3s on $server_name..."
-	ssh root@$server_name "curl -sfL https://raw.githubusercontent.com/cjrpriest/k3s-glusterfs/release-1.18/install.sh | INSTALL_K3S_VERSION=v1.18.8+k3s1+glusterfs INSTALL_K3S_EXEC=\"server --disable traefik\" sh -s - server --datastore-endpoint http://$SERVER1_IP:2379,http://$SERVER2_IP:2379,http://$SERVER3_IP:2379"
-done
-
-until $(scp -o ConnectTimeout=1 -q root@$SERVER1_NAME:/etc/rancher/k3s/k3s.yaml ~/.kube/config)
-do
-  printf .
-  sleep 1
-done
-
-echo "Modifying local kube config..."
-sed -e "s/127\.0\.0\.1:6443/$SERVER1_NAME:6443/g" -i '' ~/.kube/config
-
-echo "Deploying Metal LB..."
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f metallb-config.yml
-
-echo "Deploying k8s dashboard..."
-kubectl create -f dashboard-2.0.3-recommended.yaml
-kubectl create -f dashboard.admin-user.yml -f dashboard.admin-user-role.yml
-
-echo "Deploying gluster service and endpoints..."
-kubectl apply -f gluster-endpoints.yml
-kubectl apply -f gluster-service.yml
-
-echo "Deploying demo application..."
-kubectl apply -f nginx.yml
-
-echo "Retrieving dashboard token..."
-kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
+	echo "Retrieving dashboard token..."
+	kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
+fi
