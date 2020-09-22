c80f1f9b03886b4a8f8688b4d4db9e6dfb799ddc
Chris Priest <chris.priest@me.com>
2020-09-22 17:51:25 +0100

    assume that the partition for gluster is the last on the disk

--- install-scripts/gluster-install.sh
+++ install-scripts/gluster-install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo "Partitioning /dev/sdb on $server..."
-echo "type=83" | sfdisk /dev/sdb
-echo "Formatting /dev/sdb1 on $server..."
-mkfs.xfs -i size=512 /dev/sdb1
+# assume the partition for the gluster brick is the last on the disk
+GLUSTER_PARTITION=/dev/sda`grep -c 'sda[0-9]' /proc/partitions`
+
+echo "Formatting $GLUSTER_PARTITION on $server..."
+mkfs.xfs -i size=512 $GLUSTER_PARTITION
 mkdir -p /data/brick1
-echo '/dev/sdb1 /data/brick1 xfs defaults 1 2' >> /etc/fstab
+echo "$GLUSTER_PARTITION /data/brick1 xfs defaults 1 2" >> /etc/fstab
 mount -a && mount
 mkdir -p /data/brick1/gv0
 service glusterd start
